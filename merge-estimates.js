08b5f3e910554db751ecaea103886a18b82ad3ec
Olaf Veerman <olaf.veerman@gmail.com>
2016-04-11 14:36:51 -0400

    Add better documentation to merge script

--- merge-estimates.js
+++ merge-estimates.js
@@ -1,10 +1,28 @@
 'use strict';
+// This script uses length estimates from OSM and estimates from DPWH
+// to calculate the national and local road length by admin area.
+//
+// The output is a file with an array of objects per admin area & estimate type
+// [
+//   {
+//     "id": 17781561002,
+//     "value": 0,
+//     "measure": "national"
+//   },
+//   {
+//     "id": 17781561002,
+//     "value": 0,
+//     "measure": "local"
+//   },
+//   ...
+// ]
 
 var _ = require('lodash');
 var fs = require('fs');
 var osmEstimates = require('./data/osm-lengths.json');
 var dpwhEstimates = require('./data/dpwh-lengths.json');
 
+// National length estimates from DPWH
 var dpwhLengths = [];
 _(dpwhEstimates).forEach((o, i) => {
   dpwhLengths.push({
@@ -18,6 +36,8 @@ _(dpwhEstimates).forEach((o, i) => {
 // from the OSM estimates
 var localLengths = [];
 _(osmEstimates).forEach((o, i) => {
+  // o.totalLength = length estimate from OSM for a particular area
+  // dpwhEstimates[i].totalLength = length estimate from DPWH for the area
   let e = o.totalLength - dpwhEstimates[i].totalLength;
   localLengths.push({
     id: Number(i),
